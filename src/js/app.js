a62c4335f2eb692e609749d5d8135b66603a724b
fortavey <fortavey@gmail.com>
2020-09-23 19:36:08 +0300

    End day 23.09.2020

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
+// Header on scroll
+(function(){
+  const topLine = document.querySelector('.top-line');
+  const middleLine = document.querySelector('.middle-line');
+
+  document.addEventListener('scroll', e => {
+    if(window.scrollY > 50) { topLine.classList.add('scrolled'); middleLine.classList.add('scrolled'); }
+    else { topLine.classList.remove('scrolled'); middleLine.classList.remove('scrolled'); }
+  });
+})();
+
 // Open search form
 (function(){
   const openButton = document.querySelector('.search-open');
@@ -19,3 +30,22 @@
 
   form.querySelector('.btn').addEventListener('click', e => form.submit())
 })();
+
+//Open mobile menu
+(function(){
+  const button = document.querySelector('.open-menu');
+  const topLine = document.querySelector('.top-line');
+
+  button.addEventListener('click', e => {
+    button.classList.toggle('opened');
+    topLine.classList.toggle('opened');
+  });
+})();
+
+// Set header backgroun image
+(function(){
+  const header = document.querySelector('header.site-header');
+  const url = window.innerWidth > 500 ? header.dataset.src : header.dataset.srcMobile;
+  header.setAttribute('style', `background-image: url(${url})`)
+})();
+
