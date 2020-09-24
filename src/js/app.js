e60a2f88d227c4ce3357c9cde601f9b82a2afa3d
fortavey <fortavey@gmail.com>
2020-09-24 20:03:37 +0300

    End day 24.09.2020

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,14 @@
   const middleLine = document.querySelector('.middle-line');
 
   document.addEventListener('scroll', e => {
-    if(window.scrollY > 50) { topLine.classList.add('scrolled'); middleLine.classList.add('scrolled'); }
-    else { topLine.classList.remove('scrolled'); middleLine.classList.remove('scrolled'); }
+    if(window.scrollY > 50) {
+      topLine.classList.add('scrolled');
+      if(window.innerWidth < 900) middleLine.classList.add('scrolled');
+    }
+    else {
+      topLine.classList.remove('scrolled');
+      if(window.innerWidth < 900) middleLine.classList.remove('scrolled');
+    }
   });
 })();
 
@@ -31,21 +37,55 @@
   form.querySelector('.btn').addEventListener('click', e => form.submit())
 })();
 
-//Open mobile menu
+//Open mobile menu and click anchor link
 (function(){
   const button = document.querySelector('.open-menu');
   const topLine = document.querySelector('.top-line');
+  const lis = [...document.querySelectorAll('.top-line li')];
 
   button.addEventListener('click', e => {
     button.classList.toggle('opened');
     topLine.classList.toggle('opened');
   });
+
+  lis.forEach(li => {
+    li.addEventListener('click', e => {
+      topLine.classList.remove('opened');
+      button.classList.remove('opened');
+    });
+  });
 })();
 
-// Set header backgroun image
+// Set background image
 (function(){
-  const header = document.querySelector('header.site-header');
-  const url = window.innerWidth > 500 ? header.dataset.src : header.dataset.srcMobile;
-  header.setAttribute('style', `background-image: url(${url})`)
+  function changeBg(tag){
+    const block = document.querySelector(tag);
+    const url = window.innerWidth > 500 ? block.dataset.src : block.dataset.srcMobile;
+    block.setAttribute('style', `background-image: url(${url})`)
+  }
+
+  changeBg('header.site-header');
+  if(document.querySelectorAll('.main-page').length){
+    changeBg('.main-tech');
+  }
+
 })();
 
+// Scroll to anchore
+(function(){
+  const anchors = [...document.querySelectorAll('a[href*="#"]')];
+  anchors.forEach(anchor => {
+    anchor.addEventListener('click', e => {
+      if(document.querySelectorAll('.main-page').length){
+        e.preventDefault()
+      }
+
+      const blockID = anchor.getAttribute('href').substring(2);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+})();
